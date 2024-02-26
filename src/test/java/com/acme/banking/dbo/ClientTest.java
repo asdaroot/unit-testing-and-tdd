90d7b23c69d73f7a3e98e42934affa17e1b7afe5
asdaroot <129074366+asda101@users.noreply.github.com>
2024-02-26 15:30:16 +0300

    remove old ClientTest.java and add new w simple test V1

--- src/test/java/com/acme/banking/dbo/ClientTest.java
+++ src/test/java/com/acme/banking/dbo/ClientTest.java
@@ -16,12 +16,12 @@ import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
 
 @DisplayName("Test suite")
 public class ClientTest {
-    @Test @Disabled("temporary disabled")
+    @Test
     @DisplayName("Test case")
     public void shouldStorePropertiesWhenCreated() {
         //region given
         final int clientId = 1;
-        final String clientName = "dummy client name";
+        final String clientName = "Client Name 1";
         //endregion
 
         //region when
@@ -31,24 +31,7 @@ public class ClientTest {
 
         //region then
         //Junit5:
-        assertAll("Client store its properties",
-                () -> assertEquals(clientId, sut.getId()),
-                () -> assertEquals(clientName, sut.getName())
-        );
-
-        //Hamcrest:
-        assertThat(sut,
-            allOf(
-                hasProperty("id", notNullValue()),
-                hasProperty("id", equalTo(clientId)),
-                hasProperty("name", is(clientName))
-        ));
-
-        //AssertJ:
-        org.assertj.core.api.Assertions.assertThat(sut)
-                .hasFieldOrPropertyWithValue("id", clientId)
-                .hasFieldOrPropertyWithValue("name", clientName);
-        //also take a look at `extracting()` https://stackoverflow.com/a/51812188
-        //endregion
+        assertEquals(clientId, sut.getId());
+        assertEquals(clientName, sut.getName());
     }
 }
